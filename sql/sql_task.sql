dd6f7916e2a3a9565c5ef6879f326a44f5232486
fifajan <me@swdevelop.net>
2016-01-12 11:09:40 +0200

    Change table name.

--- sql/sql_task.sql
+++ sql/sql_task.sql
@@ -1,12 +1,14 @@
+-- SQLite query
+
 SELECT dept_id
     ,(
         SELECT COUNT(emp_id)
         FROM employee
-        WHERE employee.dept_id = department.dept_id
+        WHERE employee.dept_id = departments.dept_id
     ) AS emp_conut
     ,(
         SELECT SUM(salary)
         FROM employee
-        WHERE employee.dept_id = department.dept_id 
+        WHERE employee.dept_id = departments.dept_id 
     ) AS sal_sum
-FROM department WHERE sal_sum NOT NULL ORDER BY dept_id
+FROM departments WHERE sal_sum NOT NULL ORDER BY dept_id
